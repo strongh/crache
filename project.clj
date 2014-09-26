ed43af949376435be134d98093f69ebc1744ed82
Carlos Cunha <ccfontes@gmail.com>
2014-09-26 16:53:36 +0100

    Upgrade core.cache to get more performance
    Add license and url to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject crache "1.0.0-SNAPSHOT"
   :description "Redis-backed caching and memoization, following core.cache and core.memoize"
+  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://github.com/strongh/crache"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.cache "0.6.0"]
+                 [org.clojure/core.cache "0.6.4"]
                  [org.clojure/core.memoize "0.5.1"]
                  [org.clojars.strongh/clj-redis "0.0.20"]])
