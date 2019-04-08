8b78ece697f87e1c64546298b7a8201af377e712
Juraj Martinka <jumarko@gmail.com>
2019-04-08 15:53:01 +0200

    Bump all deps to latest stable versions

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Redis-backed caching and memoization, following core.cache and core.memoize"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "http://github.com/strongh/crache"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.taoensso/carmine "2.7.0"]
-                 [org.clojure/core.cache "0.6.4"]
-                 [org.clojure/core.memoize "0.5.6"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [com.taoensso/carmine "2.19.1"]
+                 [org.clojure/core.cache "0.7.2"]
+                 [org.clojure/core.memoize "0.7.1"]])
